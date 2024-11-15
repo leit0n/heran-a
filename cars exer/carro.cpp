c32dfd9293aa892efb1b9cb25264f8fcf06c7b01
leit0n <145568905+leit0n@users.noreply.github.com>
2024-11-15 09:15:03 -0100

    Update carro.cpp

--- cars exer/carro.cpp
+++ cars exer/carro.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // Construtor da classe Carro
 Carro::Carro(const string& nomeCarro, int portas) : Veiculo(nomeCarro), numeroDePortas(portas) {}
 
-// Sobrescreve o método mostrarDetalhes para incluir detalhes específicos do carro
+// Subrescreve o método mostrarDetalhes para incluir detalhes específicos do carro
 void Carro::mostrarDetalhes() const {
     cout << "Carro: " << nome << endl;
     cout << "Número de portas: " << numeroDePortas << endl;
